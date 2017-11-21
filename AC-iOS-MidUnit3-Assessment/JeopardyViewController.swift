826ed31a2d9ed101a9531b1a5a910da6a2a53be3
Iram Fattah <iramfattah94@gmail.com>
2017-11-21 14:09:50 -0500

    jeopardy done

--- AC-iOS-MidUnit3-Assessment/JeopardyViewController.swift
+++ AC-iOS-MidUnit3-Assessment/JeopardyViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class JeopardyViewController: UIViewController {
+class JeopardyViewController: UIViewController,UITextFieldDelegate {
 
     
-    var questions = [JeopardyQuestion]()
+    @IBOutlet weak var jeopardyImage: UIImageView!
     
-    func nextQuestion() -> String {
-        var currentIndex = 0
-        var currentQuestion = questions[currentIndex].question
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBAction func nextQuestionPressed(_ sender: UIButton) {
+        let thisQuestion = nextQuestion()
+        questionLabel.text = thisQuestion.question
+        valueLabel.text = thisQuestion.value.description
+        categoryLabel.text = thisQuestion.category
+        correctAnswer = thisQuestion.answer
+        messageLabel.text = "enter your answer"
+        answerTextField.isEnabled = true
+        answerTextField.text = ""
+        
+    }
+    var correctAnswer: String = ""
+      
+    
+    var questions = [JeopardyQuestion]()
+    var randomIndex : Int {
+        return Int(arc4random_uniform(UInt32(questions.count)))
+        
+    }
+    //var currentQuestion: JeopardyQuestion?
+    func nextQuestion() -> JeopardyQuestion {
+        var currentIndex = randomIndex
+        let currentQuestion = questions[currentIndex]
         currentIndex += 1
         if currentIndex == questions.count {
             currentIndex = 0
@@ -23,11 +53,45 @@ class JeopardyViewController: UIViewController {
         return currentQuestion
     }
     
+    func loadData() {
+        if let path = Bundle.main.path(forResource: "jeopardyinfo", ofType: "json") {
+            let myURL = URL(fileURLWithPath: path)
+            if let data = try? Data(contentsOf: myURL) {
+                self.questions = JeopardyQuestion.getJeopardyQuestions(from: data)
+            }
+        }
+    }
     
+    func loadImageAndText() {
+        
+        jeopardyImage.image = #imageLiteral(resourceName: "jeopardyLogo")
+        let initalQuestion = questions[randomIndex]
+        correctAnswer = initalQuestion.answer
+        questionLabel.text = initalQuestion.question
+        valueLabel.text = initalQuestion.value.description
+        categoryLabel.text = initalQuestion.category
+        messageLabel.text = "enter your answer"
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text?.lowercased() == correctAnswer.lowercased() {
+            messageLabel.text = "Correct!"
+            textField.isEnabled = false
+        } else {
+            messageLabel.text = "Incorrect the answer was \(correctAnswer)"
+            textField.isEnabled = false
+        }
+        textField.resignFirstResponder()
+        return true
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        answerTextField.delegate = self
+        loadData()
+        loadImageAndText()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
