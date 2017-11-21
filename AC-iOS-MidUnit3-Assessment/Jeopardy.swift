6ddbde83819222462a8343ea6c0a3ca1effae7cf
winstonmaragh@ac.c4q.nyc <WinstonMaragh@ac.c4q.nyc>
2017-11-21 17:53:25 -0500

    update

--- AC-iOS-MidUnit3-Assessment/Jeopardy.swift
+++ AC-iOS-MidUnit3-Assessment/Jeopardy.swift
@@ -5,43 +5,53 @@
 
 import Foundation
 
-enum GuessResult {
-	case correct, incorrect, almostCorrect
-}
-
-struct Jeopardy: Codable {
-	let id: Int
+class Questions {
 	let answer: String
 	let question: String
 	let value: Int?
-	let category: CategoryWrapper
-	
-}
-
-struct CategoryWrapper: Codable {
 	let title: String
+	
+	init(answer: String, question: String, value: Int?, title: String?) {
+		self.answer = answer
+		self.question = question
+		self.value = value
+		self.title = title!
+		return
+	}
+	
+	convenience init?(from jsonDict: [String: Any]) {
+		guard
+			let question = jsonDict["question"] as? String,
+			let answer = jsonDict["answer"] as? String
+		else {
+			return nil
+		}
+			let value = jsonDict["value"] as? Int ?? 0
+			var title: String?
+
+			if let categoryDict = jsonDict["category"] as? [String: String] {
+				title = categoryDict["title"]!
+			}
+		self.init(answer: answer, question: question, value: value, title: title)
+
+	}
+	
+	static func getQuestions(from data: Data) -> [Questions] {
+		var questions = [Questions]()
+		do {
+			let jsonDict = try JSONSerialization.jsonObject(with: data, options: [])
+			guard let questionsDictArr = jsonDict as? [[String:Any]] else { return []}
+			for questionsDict in questionsDictArr {
+				if let question = Questions(from: questionsDict) {
+					questions.append(question)
+				}
+			}
+		}
+		catch {
+			print("Error converting data to JSON")
+		}
+		return questions
+	}
 }
-
-
-//struct Question {
-//	private let question = []() //array of questions
-//	private let answer: String
-//	private var userGuess: String
-//
-//	mutating func guess(_ userAnswer: String) -> GuessResult {
-//		if answer == userGuess {
-//			return .correct
-//		} else if answer.contains(userGuess) {
-//			return .almostCorrect
-//		} else {
-//			return .incorrect
-//		}
-//	}
-
-//	init() {
-////		let randInt = Int(arc4random_uniform(50)) //if 50 questions - count objects in model
-////		self.answer = Questions(randInt).answer  //Questions is the array of all questions, randInt to select random question, then get the random answer
-//	}
-//}
 
 
--- AC-iOS-MidUnit3-Assessment/JeopardyVC.swift
+++ AC-iOS-MidUnit3-Assessment/JeopardyVC.swift
@@ -14,55 +14,57 @@ class JeopardyVC: UIViewController{
 	@IBOutlet weak var messageLabel: UILabel!
 	@IBOutlet weak var scoreLabel: UILabel!
 	
-	//Actions
-	@IBAction func nextQuestionPressed(_ sender: UIButton) {
+	@IBAction func press(_ sender: UIButton) {
+	}
+	@IBAction func nextQuestionPress(_ sender: UIButton) {
 		loadQuestion()
 
 	}
+
 	//MARK: - Variables/constants
-	var questions = [Jeopardy]()
+	var questions = [Questions]()
 	var numOfTotalQuestions: Int = 0
 	var randInt: Int = 0
-	var currentQuestion: Jeopardy!
+	var currentQuestion: Questions!
 	var playerScore: Int = 0
 	var answer: String = ""
 	
 	//MARK: - Overrides
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		loadData()
 		loadQuestion()
 		playerScore = 0
 		numOfTotalQuestions = questions.count
 		randInt = Int(arc4random_uniform(UInt32(numOfTotalQuestions)))
-		
+	}
+	
+	func loadData() {
+		if let path = Bundle.main.path(forResource: "jeopardyinfo", ofType: "json") {
+			let myURL = URL(fileURLWithPath: path)
+			if let data = try? Data(contentsOf: myURL) {
+				let questions = Questions.getQuestions(from: data)
+				self.questions = questions
+			}
+		}
+		/*
+		for question in questions {
+			print(question.question)
+		}
+		*/
 	}
 	
 	func loadQuestion(){
 		numOfTotalQuestions = questions.count
 		currentQuestion = questions[randInt]
-		categoryLabel.text =  "Category: \(currentQuestion.category.title)"
+		categoryLabel.text =  "Category: \(currentQuestion.title)"
 		questionLabel.text = "Question: \(currentQuestion.question)"
 		valueLabel.text = "Value: \(currentQuestion.value ?? 0)"
 		scoreLabel.text = "Score: \(playerScore)"
 		messageLabel.text = ""
+		
 		messageLabel.isHidden = true
 		answer = currentQuestion.answer
-	}
-	
-	//MARK: - Functions
-	func loadJeopardyData() {
-		if let path = Bundle.main.path(forResource: "jeopardyinfo", ofType: "json") {
-			let url = URL(fileURLWithPath: path)
-			if let data = try? Data(contentsOf: url) {
-				do {
-					let jeopardy = try JSONDecoder().decode(Jeopardy.self, from: data)
-					self.currentQuestion = jeopardy
-				}
-				catch {
-					print(error)
-				}
-			}
-		}
 	}
 	
 }
