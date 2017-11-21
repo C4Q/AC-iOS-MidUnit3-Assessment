d3ed7590cd7e289539c28c964e2c610939967cdf
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2017-11-21 15:25:17 -0500

    missing textfield delegates for Jeopardy. Cant get randomizer to work

--- AC-iOS-MidUnit3-Assessment/JeopardyModel.swift
+++ AC-iOS-MidUnit3-Assessment/JeopardyModel.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+class GameBrain {
+    var currentQuestion: String = ""
+    var currentAnswer: String = ""
+    var currentValue: Int? = 0
+    var currentCategory: String = ""
+}
+
+class Questions {
+    var question: String
+    var value: Int?
+    var answer: String
+    var category: String
+    init(question: String, value: Int?, answer: String, category: String) {
+        self.answer = answer
+        self.value = value
+        self.question = question
+        self.category = category
+    }
+    convenience init?(from dict: [String: Any]) {
+        let question = dict["question"] as? String ?? "Mystery Question"
+        let value = dict["value"] as? Int ?? 0
+        guard let answer = dict["answer"] as? String else {return nil}
+        guard let category = dict["category"] as? [String: Any] else {return nil}
+        let title = category["title"] as? String ?? "Mystery Category"
+        self.init(question: question, value: value, answer: answer, category: title)
+    }
+    static func getQuestions(from data: Data) -> [Questions] {
+        var questions = [Questions]()
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            guard let jsonDict = json as? [[String: Any]] else {return [] }
+            for questionDict in jsonDict {
+                if let newQuestion = Questions(from: questionDict) {
+                    questions.append(newQuestion)
+                }
+            }
+        } catch {
+            print("JSON Serializatin did not work buddy")
+        }
+        return questions
+    }
+}
+
+
+
--- AC-iOS-MidUnit3-Assessment/JeopardyViewController.swift
+++ AC-iOS-MidUnit3-Assessment/JeopardyViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class JeopardyViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    
     @IBOutlet weak var categoryLabel: UILabel!
     
     @IBOutlet weak var valueLabel: UILabel!
@@ -19,33 +20,40 @@ class JeopardyViewController: UIViewController {
     @IBOutlet weak var messageToUser: UILabel!
     
     @IBAction func nextQuestionButton(_ sender: UIButton) {
-        
+        nextQuestion()
+        categoryLabel.text = gameBrain.currentCategory
+        messageToUser.text = "Guess The Answer!"
+        valueLabel.text = "Value: \(gameBrain.currentValue ?? 0)"
+        questionLabel.text = gameBrain.currentQuestion
     }
     
     @IBOutlet weak var texfield: UITextField!
     
-    
-    
+    var jeopardyQuestions = [Questions]()
+    var gameBrain = GameBrain()
+    var counter = 0
+    func nextQuestion() {
+        gameBrain.currentQuestion = jeopardyQuestions[counter].question
+        gameBrain.currentAnswer = jeopardyQuestions[counter].answer
+        gameBrain.currentValue = jeopardyQuestions[counter].value
+        gameBrain.currentCategory = jeopardyQuestions[counter].category
+        counter += 1
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        loadData()
+        nextQuestion()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func loadData() {
+        if let path = Bundle.main.path(forResource: "jeopardyinfo", ofType: "json") {
+            let myURL = URL(fileURLWithPath: path)
+            if let data = try? Data(contentsOf: myURL) {
+                self.jeopardyQuestions = Questions.getQuestions(from: data)
+            } else {
+                print("JSON Serialization didn't work. Git Gud")
+            }
+        }
     }
-    */
-
 }
