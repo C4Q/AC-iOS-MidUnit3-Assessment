4c046b6f32637be744c1b78037db89b82b3114e0
Xianxian Chen <xianxianchen@ac.c4q.nyc>
2017-11-21 16:26:10 -0500

    final change

---
+++ AC-iOS-MidUnit3-Assessment/BookData.swift
@@ -0,0 +1,9 @@
+//
+//  BookData.swift
+//  AC-iOS-MidUnit3-Assessment
+//
+//  Created by C4Q on 11/21/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
---
+++ AC-iOS-MidUnit3-Assessment/JeopardyData.swift
@@ -0,0 +1,9 @@
+//
+//  JeopardyData.swift
+//  AC-iOS-MidUnit3-Assessment
+//
+//  Created by C4Q on 11/21/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
